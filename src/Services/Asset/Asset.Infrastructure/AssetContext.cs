804dc1a80bc9d1f155d14ef9dfbbaa5ef4a7bd22
MihaelPavlov <57910640+MihaelPavlov@users.noreply.github.com>
2022-10-19 08:53:55 +0300

    create clean architecture project structure

---
+++ src/Services/Asset/Asset.Infrastructure/AssetContext.cs
@@ -0,0 +1,22 @@
+﻿namespace Asset.Infrastructure
+{
+    using Microsoft.EntityFrameworkCore;
+
+    public class AssetContext : DbContext
+    {
+        public AssetContext(DbContextOptions<AssetContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+#if DEBUG
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("server=(local); database=KAS.Database.Asset; Integrated Security=true");
+            }
+#endif
+        }
+    }
+}
---
+++ src/Services/Asset/Asset.Infrastructure/DI.cs
@@ -0,0 +1,13 @@
+﻿namespace Asset.Infrastructure
+{
+    using Microsoft.Extensions.DependencyInjection;
+
+    public static class DI
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services)
+        {
+
+            return services;
+        }
+    }
+}
