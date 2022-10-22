bc73130c66fd97b9f704042fb7446c596febfd85
MihaelPavlov <57910640+MihaelPavlov@users.noreply.github.com>
2022-10-22 10:32:24 +0300

    clean asset api project

---
+++ src/Services/Asset/Asset.API/Controllers/AssetController.cs
@@ -0,0 +1,27 @@
+﻿namespace Asset.API.Controllers
+{
+    using Asset.Application.Persistence;
+    using Asset.Domain.Entities;
+    using Microsoft.AspNetCore.Mvc;
+    using System.Net;
+
+    [ApiController]
+    [Route("[controller]")]
+    public class AssetController : ControllerBase
+    {
+        private readonly IAssetRepository assetRepository;
+
+        public AssetController(IAssetRepository assetRepository)
+        {
+            this.assetRepository = assetRepository;
+        }
+
+        [HttpGet("assetId")]
+        [ProducesResponseType(typeof(Asset), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<Asset>> GetAssetById(int assetId)
+        {
+            var result = await this.assetRepository.GetByIdAsync(assetId);
+            return this.Ok(result);
+        }
+    }
+}
