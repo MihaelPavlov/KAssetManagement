f182c09c70466f1a3d5066885ad91934fac60c7b
MihaelPavlov <57910640+MihaelPavlov@users.noreply.github.com>
2022-11-01 19:37:12 +0200

    create separate project for all bus events

---
+++ src/Shared/EventBus.Messages/AssetEvents/CreateAssetLocationEvent.cs
@@ -0,0 +1,11 @@
+﻿namespace EventBus.Messages.AssetEvents
+{
+    using EventBus.Messages.Common;
+
+    public class CreateAssetLocationEvent : IntegrationBaseEvent
+    {
+        public int AssetId { get; set; }
+        public int LocationId { get; set; }
+        public int UpdatedBy { get; set; }
+    }
+}
---
+++ src/Shared/EventBus.Messages/Common/IntegrationBaseEvent.cs
@@ -0,0 +1,21 @@
+﻿namespace EventBus.Messages.Common
+{
+    public class IntegrationBaseEvent
+    {
+        public IntegrationBaseEvent()
+        {
+            this.Id = Guid.NewGuid();
+            this.CreationDate = DateTime.Now;
+        }
+
+        public IntegrationBaseEvent(Guid id, DateTime creationDate)
+        {
+            Id = id;
+            CreationDate = creationDate;
+        }
+
+        public Guid Id { get; private set; }
+
+        public DateTime CreationDate { get; private set; }
+    }
+}
